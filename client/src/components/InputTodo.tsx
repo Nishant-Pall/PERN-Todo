b97a28f490cec47cf17b84e6b4b85cf9283e2d24
Nishant-Pall <palln6935@gmail.com>
2021-07-22 13:09:30 +0530

    Remove lint flags

--- client/src/components/InputTodo.tsx
+++ client/src/components/InputTodo.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { Fragment, useState, useRef, useEffect } from "react";
 
 const InputTodo = () => {
--- client/src/components/ListTodo.tsx
+++ client/src/components/ListTodo.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { Fragment, useEffect, useState } from "react";
 
 const ListTodo = () => {
